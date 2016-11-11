8857f5977331aa99307d10dc198a27b80718c097
Enexl-Naveen <naveen.b@enexl.com>
2016-11-11 14:12:19 +0530

    11.30

--- HelloEnexl/src/Hello.java
+++ HelloEnexl/src/Hello.java
@@ -6,14 +6,7 @@ public class Hello {
 		
 		System.out.println("hello all");
 		
-		System.out.println("hi at 1.23pm i edited some code.");
-		
-		
-		
-		System.out.println("i am rakshith complted  my task at 11.30");
-		
-		
-		System.out.println("hi i am naveen i complted task at 2.30");
+
 		
 	}
 
